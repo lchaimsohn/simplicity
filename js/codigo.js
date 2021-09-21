b3980f3a12d71d5bb5cebfbf9c94858d42ed7d7d
Lucas Chaimsohn <69763624+lchaimsohn@users.noreply.github.com>
2021-09-21 11:34:46 -0300

    Upload dos arquivos do site Simplicity
    Site Simplicity para teste e aprendizado.

---
+++ js/codigo.js
@@ -0,0 +1,14 @@
+const botaoMenu = document.querySelector(".icone");
+const menu = document.querySelector(".menu");
+
+
+botaoMenu.addEventListener("click", function(event){
+    event.preventDefault();
+    menu.classList.toggle("menu-aberto");
+    if(menu.classList.contains("menu-aberto") ){
+        botaoMenu.innerHTML = "Fechar &times;";
+    }
+    else{
+        botaoMenu.innerHTML = "Menu &equiv;";
+    }
+})
